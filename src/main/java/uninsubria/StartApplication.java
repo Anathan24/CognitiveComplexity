ff8131b1b5cd5e4c5a55245d479dc76fb7832773
Anatoliy Roshka <roshka.anatoliy@gmail.com>
2021-03-14 11:08:27 +0100

    Changes input format

--- src/main/java/uninsubria/StartApplication.java
+++ src/main/java/uninsubria/StartApplication.java
@@ -17,16 +17,16 @@ import uninsubria.cognitivecomplexity.dao.ModuleInfoDAO;
 public class StartApplication {
 
 	private static final Logger logger = LogManager.getLogger();
-	private static final BufferedReader readLine = new BufferedReader(new InputStreamReader(System.in));
 	private File directoryOrFile = null;
 	private File outputDirectory = null;
 	private String outputFileName = null;
 	private List<List<ModuleInfoDAO>> calculusResults;
 	
-	private StartApplication() throws IOException {
-		directoryOrFile = this.askForInputFiles();
-		outputDirectory = this.askForOutputDirectory();
-		outputFileName = this.askForOutputFileName();
+	private StartApplication(String directoryOrFilePath, String outputDirectoryPath, String outputFileName) throws IOException {
+		directoryOrFile = checkExistingInputDirectory(directoryOrFilePath);
+		outputDirectory = checkexistingOutputDirectory(outputDirectoryPath);
+		this.outputFileName = outputFileName;
+		
 		System.out.println();//Questa riga serve solo per serapare due blocchi di testo in console
 		calculusResults = new InputManager().executeCognitiveComplexityCalculus(directoryOrFile);
 		System.out.println();//Questa riga serve solo per serapare due blocchi di testo in console
@@ -38,113 +38,56 @@ public class StartApplication {
 		}
 	}
 	
-	private File askForInputFiles() throws IOException {
-		String responce = null;
+	private File checkExistingInputDirectory(String directoryOrFilePath) throws IOException {
 		File result = null;
-		
-		try {
-			do{
-				responce = "N";
-				logger.info("Step 1: Enter the path to File or Directory:");
-				String inputLine = readLine.readLine();
-				
-				File directoryOrJavaFile = new File(inputLine);
-				if(directoryOrJavaFile.exists()) {
-					result = directoryOrJavaFile;
-				}else if(directoryOrJavaFile.isFile()? directoryOrJavaFile.getName().endsWith(".java") : false){
-					result = directoryOrJavaFile;
-				}else {
-					logger.info("If you try to analyze a java file, check that the name of file end with file extension(.java)");
-					logger.info("Directory/File does not exist!");
-					logger.info("Retry (S/N) ?");
-					responce = readLine.readLine();
-				}
-				
-				if((responce.equals("N") || responce.equals("n")) && result == null) {
-					throw new IOException("Specified Directory/File does not exist!");
-				}
-				
-			}while(responce.equals("S") || responce.equals("s"));
-		} catch (IOException e) {
-			readLine.close();
-			e.printStackTrace();
-			System.exit(0);
+		File directoryOrJavaFile = new File(directoryOrFilePath);
+		if(directoryOrJavaFile.isDirectory()) {
+			result = directoryOrJavaFile;
+		}else if(directoryOrJavaFile.isFile()? directoryOrJavaFile.getName().endsWith(".java") : false){
+			result = directoryOrJavaFile;
+		}else {
+			logger.info("Execution Stopped! An error has occurred");
+			logger.info("If you try to analyze a java file, check the name of file ends with file extension(.java)");
+			logger.info("If you try to analize a directory, check the directory exist or the path is written correctly");
+			System.out.println();
+			throw new IOException("File or Directory does not exist! - "+directoryOrFilePath);
 		}
 		
 		return result;		
 	}
 	
-	private File askForOutputDirectory() throws IOException {
-		String responce = null;
+	private File checkexistingOutputDirectory(String outputDirectoryPath) throws IOException {
 		File result = null;
 		
-		try {
-			do{
-				responce = "N";
-				logger.info("Step 2: Enter the path to the output directory:");
-				String inputLine = readLine.readLine();
-				
-				File directory = new File(inputLine);
-				if(directory.exists()) {
-					result = directory;
-				}else {
-					logger.info("Directory does not exist!");
-					logger.info("Retry (S/N) ?");
-					responce = readLine.readLine();
-				}
-				
-				if((responce.equals("N") || responce.equals("n")) && result == null) {
-					throw new IOException("Specified Directory does not exist!");
-				}
-			}while(responce.equals("S") || responce.equals("s"));
-		} catch (IOException e) {
-			readLine.close();
-			e.printStackTrace();
-			System.exit(0);
+		File directory = new File(outputDirectoryPath);
+		if(directory.exists()) {
+			result = directory;
+		}else {
+			logger.info("Execution Stopped! An error has occurred");
+			throw new IOException("Directory does not exist! - "+outputDirectoryPath);
 		}
 		
 		return result;
 	}
 	
-	private String askForOutputFileName() throws IOException {
-		String responce = null;
-		String result = null;
-		
-		try {
-			responce = "N";
-			logger.info("Step 3: Enter output file name:");
-			String fileName = readLine.readLine();
-			
-			if(!(fileName == null)) {
-				result = fileName;
-			}else {
-				logger.info("File name can not be null!");
-				logger.info("Retry (S/N) ?");
-				responce = readLine.readLine();
-			}
-			
-			if((responce.equals("N") || responce.equals("n")) && result == null) {
-				throw new IOException("FileName can not be null!");
-			}
-		} catch (IOException e) {
-			readLine.close();
-			e.printStackTrace();
-			System.exit(0);
-		}
-		
-		return result;
-	}
 	
 	public static void main(String[] args) throws IOException  {
 		try {
 			logger.info("START");
-			new StartApplication();
+			if(args.length < 3) {
+				logger.info("Execution Stopped! An error has occurred: application must receive three arguments in input!");
+				logger.info("First one: the path to file/directory to analize");
+				logger.info("Secone one: the path to output directory in which to create the result file");
+				logger.info("Third one: the name of output csv file that will be contain a calculation results");
+				System.out.println();
+				throw new IOException("Insufficient arguments amount!");
+			}else {
+				new StartApplication(args[0], args[1], args[2]);
+			}
+			
 			logger.info("END");
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
-			readLine.close();
 		}
-		
 	}
 }
